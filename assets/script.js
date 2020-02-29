3b4ad90cee6a80b1eb88e046a4ea65cfed92c1b6
Lionel Jouin <lionel.jouin@outlook.com>
2020-02-29 17:51:15 +0100

    New Theme

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-var something = document.getElementById('canvasSection');
-var pattern = Trianglify({
-    width: window.innerWidth,
-    height: window.innerHeight
-});
-something.appendChild(pattern.canvas())
